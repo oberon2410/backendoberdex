e81722c24092fa244efbbdbfc535def3c3a1b83c
oberon2410 <31382852+oberon2410@users.noreply.github.com>
2018-09-26 00:22:36 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from os import environ
 HTTP_PROVIDER_URL = environ.get("HTTP_PROVIDER_URL")
 WS_PROVIDER_URL = environ.get("WS_PROVIDER_URL")
 
-ALLOWED_ORIGIN_SUFFIXES = ('localhost', 'bitox.io', 'www.bitox.io', 'backend.bitox.io')
+ALLOWED_ORIGIN_SUFFIXES = ('localhost', 'http://trade.oberdex.org/', 'trade.oberdex.org', 'backendoberdex.herokuapp.com')
 ED_CONTRACT_ADDR = '0xb5adb233f28c86cef693451b67e1f2d41da97d21'
 with open('bitox.abi.json') as f:
     import json
